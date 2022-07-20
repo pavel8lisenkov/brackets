9e051426477b45a40d8a948c7ea45657baadc773
Pasha <pavel8lisenkov@yandex.ru>
2022-07-21 00:04:10 +0400

    tasc complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let opensBrack = [];
+
+  bracketsConfig.forEach(function(elem) {
+    opensBrack.push(elem[0]);
+  })
+
+  let closetsBrack = [];
+
+  bracketsConfig.forEach(function(elem) {
+    closetsBrack.push(elem[1]);
+  })
+
+  let brackObj = {};
+
+  bracketsConfig.forEach(function(elem) {
+    let key = elem[1];
+    let value = elem[0];
+    brackObj[key] = value;
+  })
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+
+    let currentBrack = str[i];
+    let topStack = stack[stack.length - 1];
+
+    if (topStack === currentBrack && closetsBrack.includes(currentBrack)) {
+      stack.pop();
+    } else {
+      if (opensBrack.includes(currentBrack)) {
+        stack.push(currentBrack);
+      } else {
+
+        if (stack.length === 0) {
+          return false;
+        }
+
+        if (brackObj[currentBrack] === topStack) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
